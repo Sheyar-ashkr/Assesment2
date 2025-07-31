219f730e2603e5da2f48c31a1272ed6bc9be072e
Sheyar-ashkr <sheyar9999@hotmail.com>
2025-07-31 11:29:32 +0200

    HTML Elemnte aus index.html holen

---
+++ script.js
@@ -0,0 +1,6 @@
+//Erstemal html elemente holen
+
+const form = document.getElementById("recipe-form");
+const list = document.getElementById("recipe-list");
+const searchInput = document.getElementById("search");
+const categoryFilter = document.getElementById("category-filter");
